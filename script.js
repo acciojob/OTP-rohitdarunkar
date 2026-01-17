f5cbd571b4fc9ea49622c23a014905e16ba3d12b
Rohit Darunkar <139346362+rohitdarunkar@users.noreply.github.com>
2026-01-17 21:58:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+// Focus first input on load
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+  code.addEventListener('keydown', (e) => {
+    // Allow only digits and Backspace
+    if (e.key >= '0' && e.key <= '9') {
+      code.value = '';
+      setTimeout(() => {
+        if (codes[idx + 1]) {
+          codes[idx + 1].focus();
+        }
+      }, 10);
+    } 
+    else if (e.key === 'Backspace') {
+      setTimeout(() => {
+        if (codes[idx - 1]) {
+          codes[idx - 1].focus();
+        }
+      }, 10);
+    }
+  });
+});
